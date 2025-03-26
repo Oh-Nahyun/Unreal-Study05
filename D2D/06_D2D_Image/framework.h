173f3939ce2172d5b4690dc45272b7e1f5c6fcd8
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2025-03-26 20:08:19 +0900

    Update framework.h

--- D2D/06_D2D_Image/framework.h
+++ D2D/06_D2D_Image/framework.h
@@ -13,17 +13,18 @@
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
-#include <math.h>			/////
-#include <map>				/////
-#include <iostream>			/////
+#include <math.h>							/////
+#include <map>								/////
+#include <iostream>							/////
 
 ///// DX Header Files
-#include <d2d1.h>			/////
-#include <d2d1helper.h>		/////
-#include <dwrite.h>			/////
-#include <wincodec.h>		/////
+#include <d2d1.h>							/////
+#include <d2d1helper.h>						/////
+#include <dwrite.h>							/////
+#include <wincodec.h>						/////
 
 ///// trace
+#include "Trace.h"							/////
 
 #pragma comment(lib, "d2d1.lib")			/////
 #pragma comment(lib, "dwrite.lib")			/////
